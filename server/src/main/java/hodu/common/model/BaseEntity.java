5273875578aca654d15ae59815c2001ad013dfa4
Woojae Jeong <woo6388@naver.com>
2024-12-09 00:00:44 +0900

    refactor: BaseEntity 어노테이션 추가

--- server/src/main/java/hodu/common/model/BaseEntity.java
+++ server/src/main/java/hodu/common/model/BaseEntity.java
@@ -1,10 +1,15 @@
 package hodu.common.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.sql.Timestamp;
 
+@MappedSuperclass
+@EntityListeners(value = {AuditingEntityListener.class})
 public class BaseEntity {
     @CreatedDate
     @Column(nullable = false, updatable = false)
