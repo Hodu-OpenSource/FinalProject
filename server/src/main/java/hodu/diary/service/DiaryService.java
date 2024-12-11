4db1e336589acb94131cff3f8e9b1ae7956aa32e
Woojae Jeong <woo6388@naver.com>
2024-12-11 19:34:05 +0900

    feat: 자바에서 파이썬 코드 실행하는 기능 구현

--- server/src/main/java/hodu/diary/service/DiaryService.java
+++ server/src/main/java/hodu/diary/service/DiaryService.java
@@ -3,22 +3,58 @@ package hodu.diary.service;
 import hodu.diary.domain.Diary;
 import hodu.diary.dto.DiaryDTO;
 import hodu.diary.repository.DiaryRepository;
+import hodu.member.domain.Member;
+import hodu.member.exception.MemberNotFoundException;
+import hodu.member.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 
 @Service
 public class DiaryService {
     private final DiaryRepository diaryRepository;
+    private final MemberRepository memberRepository;
+    private final String scriptPath = "../python_diary/main.py";
 
-    public DiaryService(DiaryRepository diaryRepository) {
+    public DiaryService(DiaryRepository diaryRepository, MemberRepository memberRepository) {
         this.diaryRepository = diaryRepository;
+        this.memberRepository = memberRepository;
     }
 
     public void addDiary(Long memberId) {
-        //일기 생성 파이썬 코드
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(()->new MemberNotFoundException("Id : " + memberId + "인 멤버를 찾을 수 없습니다"));
+        
+        try{
+            ProcessBuilder processBuilder = new ProcessBuilder("python", scriptPath, Long.toString(memberId));
+            processBuilder.redirectErrorStream(true); // stderr를 stdout에 병합
 
-       //diaryRepository.save(new Diary()) 일기 저장 코드
+            processBuilder.environment().put("PYTHONIOENCODING", "UTF-8"); // 한글이 정상적으로 저장되고 출력되도록 PYTHONIOENCODING 환경 변수 설정
+
+            Process process = processBuilder.start();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+            while((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            int exitCode = process.waitFor();
+            if (exitCode == 0) {
+                System.out.println("파이썬 스크립트 정상적으로 종료");
+            } else {
+                System.err.println("파이썬 스크립트 비정상적인 종료. exitCode : " + exitCode);
+            }
+
+        } catch (IOException e) {
+            System.err.println("파이썬 스크립트에서 에러발생: " + e.getMessage());
+        } catch (InterruptedException e) {
+            System.err.println("프로세스 인터럽트 발생 : " + e.getMessage());
+        }
 
     }
 
